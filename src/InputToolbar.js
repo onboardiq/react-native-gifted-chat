18244abdcccaca16bd5534a427b080220001da34
Ken Hibino <ken.hibino7@gmail.com>
2018-06-15 07:14:54 -0700

    Fix input border color

--- src/InputToolbar.js
+++ src/InputToolbar.js
@@ -82,7 +82,7 @@ export default class InputToolbar extends React.Component {
   }
 
   render() {
-    const isFocused = this.state.position === 'absolute';
+    const isFocused = this.state.position === 'relative';
     return (
       <View style={[styles.container, this.props.containerStyle, { position: this.state.position }]}>
         <View style={[styles.primary, this.props.primaryStyle, isFocused && styles.primaryFocused]}>
